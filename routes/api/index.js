1331d7fcd39ef119887dadccd0a2318e8992ce28
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-03 13:52:36 -0600

    Created the basic structure for the files and some of the connections to those files

---
+++ routes/api/index.js
@@ -0,0 +1,15 @@
+// Set requirements (Express Router)
+const router = require('express').Router();
+
+// Set routes (user and thought routes)
+const usersRoutes = require('./userRoutes');
+const thoughtsRoutes = require('./thoughtRoutes');
+
+// Add `/users` to created routes 
+router.use('/users', usersRoutes);
+
+// Add `/thoughts` to created routes 
+router.use('/thoughts', thoughtsRoutes);
+
+// Export Module Router
+module.exports = router;
