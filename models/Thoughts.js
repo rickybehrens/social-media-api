cd74cbcfbfb75bf88b2ca3d2c41ae8cf57353f68
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-03 14:38:35 -0600

    Created the thoughts model but once again Im struggling with the relationship of thoughts and reactions....

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -3,6 +3,27 @@ const { Schema, model } = require('mongoose');
 // Schema to create Users model
 const usersSchema = new Schema(
     {
-        
+        thoughtId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        thought: {
+            type: String,
+            required: true,
+            maxlength: 50,
+            minlength: 4,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+    },
+    {
+        toJSON: {
+            getters: true,
+          },
+          id: false,
     }
-)
+);
+
+module.exports = usersSchema;
