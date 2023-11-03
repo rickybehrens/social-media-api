08ee1677d831423833224a11d545426501297b5c
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-03 14:49:13 -0600

    Tried creating the reactions part but Im not sure if its correct or not. Decided to just create it as I thought and then debug as I go along

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,29 +1,73 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create Users model
-const usersSchema = new Schema(
+// ReactionsSchema
+const reactionsSchema = new Schema(
+    {
+        // Set custom ID 
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionText: {
+            type: String,
+            required: true,
+            maxlength: 50
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Schema to create Thoughts model
+const thoughtsSchema = new Schema(
     {
         thoughtId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
         },
-        thought: {
+        thoughtText: {
             type: String,
             required: true,
             maxlength: 50,
             minlength: 4,
         },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionsSchema],
         createdAt: {
             type: Date,
             default: Date.now,
-          },
+        },
     },
     {
         toJSON: {
             getters: true,
-          },
-          id: false,
+        },
+        id: false,
     }
 );
 
-module.exports = usersSchema;
+// get total count of reactions
+thoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// create the Thoughts model using the Thoughts Schema
+const Thoughts = model('Thoughts', thoughtsSchema);
+
+
+module.exports = Thoughts;
