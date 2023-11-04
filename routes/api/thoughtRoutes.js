881b878175d30815c73dfdeccb1e9af7e41b627a
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-04 15:54:20 -0600

    Created the routes for Users

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,20 +2,20 @@ const router = require('express').Router();
 
 const {
     getThoughts,
-    getSIngleThought,
+    getSingleThought,
     createThought,
     updateThought,
     deleteThought,
+    createReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtsController.js');
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/thoughts/thoughtId
-router
-.route('/:thoughtId')
-.get(getSIngleThought)
-.put(updateThought)
-.delete(deleteThought);
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reaction/:reactionId
+router.route('/:thoughtId/reaction/:reactionId').post(createReaction).delete(deleteReaction);
 module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/username
+router.route('/:username').get(getSingleUser).delete(deleteUser);
+
+// /api/users/:username/friends/:username
+router.route('/:username/friends/:username').post(addFriend);
+
+// /api/users/:username/friends/:username
+router.route('/:username/friends/:username').delete(deleteFriend);
+
+module.exports = router;
