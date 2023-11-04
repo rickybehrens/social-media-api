01bee29d76bac9eebc30b75047066ab4423cd655
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-04 16:56:52 -0600

    I tried to complete the controller file for users but I really don't know if they are correct or not...

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const { Users, Thoughts } = require('../models');
 
 module.exports = {
     // Get all Users
-    async getUsers (req, res) {
+    async getUsers(req, res) {
         try {
             const users = await Users.find();
             res.json(users);
@@ -16,7 +16,7 @@ module.exports = {
     async getSingleUser(req, res) {
         try {
             const user = await Users.findOne({ _id: req.params.usedname })
-            .select('-__v');
+                .select('-__v');
 
             if (!user) {
                 return res.status(404).json({ message: 'No user with that username' });
@@ -72,6 +72,61 @@ module.exports = {
     },
 
     // Add a single Friend
+    async addFriend(req, res) {
+        try {
+            const { username, friendUsername } = req.params;
+            const user = await Users.findOne({ _id: username });
+            const friend = await Users.findOne({ username: friendUsername });
+
+            if (!user || !friend) {
+                return res.status(404).json({ message: 'User or friend not found' });
+            }
+
+            if (user._id.equals(friend._id)) {
+                return res.status(400).json({ message: 'You cannot be friends with yourself' });
+            }
+
+            // Check if the friend is already in the user's friends list
+            if (user.friends.includes(friend._id)) {
+                return res.status(400).json({ message: 'Friend already added' });
+            }
+
+            user.friends.push(friend._id);
+            await user.save();
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 
     // Delete a single Friend
+    async deleteFriend(req, res) {
+        try {
+            const { username, friendUsername } = req.params;
+            const user = await Users.findOne({ _id: username });
+
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+
+            // Check if the friend is in the user's friends list
+            const friend = await Users.findOne({ username: friendUsername });
+            if (!friend) {
+                return res.status(404).json({ message: 'Friend not found' });
+            }
+
+            if (!user.friends.includes(friend._id)) {
+                return res.status(400).json({ message: 'Friend not found in the user\'s friends list' });
+            }
+
+            user.friends = user.friends.filter((id) => !id.equals(friend._id));
+            await user.save();
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
 }
