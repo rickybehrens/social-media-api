8c48d4bcd3b07a131fd14a47aceecf70a98540e6
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-07 14:12:40 -0700

    Im having issue with deleting the though id from the users thought array once it is deleted. The thought is deleted but the id number stays and it is not pulled from the users information. not sure whats happening but Im having the same issue with the reactions

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -37,7 +37,7 @@ module.exports = {
             console.log(thought);
             const userData = await Users.findOneAndUpdate(
                 { _id: req.body.userId },
-                { $push: {thoughts: thought._id }},
+                { $push: { thoughts: thought._id } },
                 { runValidators: true, new: true }
             )
             console.log(userData);
@@ -59,7 +59,7 @@ module.exports = {
             );
 
             if (!thought) {
-                res.status(404).json({ message: 'No thought with this id!' });
+                res.status(404).json({ message: 'No thought with this ID' });
             }
 
             res.json(thought);
@@ -71,11 +71,27 @@ module.exports = {
     // Delete a single Thought
     async deleteThought(req, res) {
         try {
+            // First, find and delete the thought
             const thought = await Thoughts.findOneAndDelete({ _id: req.params.thoughtId });
 
             if (!thought) {
-                res.status(404).json({ message: 'No thought with that ID' });
+                return res.status(404).json({ message: 'No thought with that ID' });
             }
+
+            // Then, remove the thoughtId from the user's thoughts array
+            const userData = await Users.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $pull: { thoughts: thought._id } }, // Use $pull to remove the thoughtId from the array
+                { runValidators: true, new: true }
+            );
+
+            if (!userData) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+            
+            // Send a success response
+            res.status(200).json({ message: 'Thought deleted successfully' });
+
         } catch (err) {
             res.status(500).json(err);
         }
